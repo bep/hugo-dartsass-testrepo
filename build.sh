821011a9c6087eea2f96cb86c9c1f4b47681e79d
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2022-03-09 09:49:36 +0100

    Get it to build on Netlify ...

--- build.sh
+++ build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+echo "Install Dart Sass Embedded..."
+
+# This is in Netlify's PATH.
+BIN_DIR=/opt/build/repo/node_modules/.bin
+DARTSASS_VERSION=1.49.9
+
+mkdir -p $BIN_DIR
+
+curl -LJO https://github.com/sass/dart-sass-embedded/releases/download/${DARTSASS_VERSION}/sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+tar -xvf sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+mv sass_embedded/dart-sass-embedded $BIN_DIR
+
+rm -rf sass_embedded*;
+
+echo "List Bin Dir..."
+
+ls $BIN_DIR;
+
+dart-sass-embedded --version
+
 echo "Building..."
 
-echo "Path is: $PATH"
+hugo
